673f0a25193ae5e6b87030b1b8e09aa38094bc11
Wouter Geraedts <git@woutergeraedts.nl>
2014-06-28 16:20:21 +0200

    Cleaned up whitespace, definitions

--- prop.ml
+++ prop.ml
@@ -2,10 +2,7 @@
 
 let unique xs = List.fold_left (fun res x -> if List.mem x res then res else x::res) [] xs (* TODO more efficient *)
 let empty l = (List.length l) = 0
-let implode f sep xs =
-    if empty xs then ()
-    else (f (List.hd xs) ; List.iter (fun x -> print_string sep ; f x) (List.tl xs))
-    
+let implode f sep xs = if not (empty xs) then (f (List.hd xs) ; List.iter (fun x -> print_string sep ; f x) (List.tl xs))
 
 (* KERNEL *)
 
@@ -44,13 +41,12 @@ module LCF : LCF_kernel = struct
 
     let assume a = Sequent([a], a)
     let intro_rule a (Sequent(benv, b)) = Sequent(List.filter (fun x -> a <> x) benv, Impl(a, b))
-    let elim_rule athm (Sequent(cenv, c)) =
-        match athm with
-            (Sequent(aenv, Impl(a, b))) ->
-                if a = c
-                then Sequent(unique (List.append aenv cenv), b)
-                else failwith("elim_rule, not of form (a -> b, a)")
-          | _ -> failwith("elim_rule, not of form (a -> b, a)")
+    let elim_rule (Sequent(env0, f)) (Sequent(env1, a1)) =
+        match f with
+            Impl(a0, b) -> if a0 = a1
+                then Sequent(unique (List.append env0 env1), b)
+                else failwith("elim_rule (a0 -> b, a1), where a0 != a1")
+          | _ -> failwith("elim_rule, left hand side not an Impl")
     
     let is_var =
         function
@@ -83,17 +79,18 @@ include LCF (* Include sealed kernel *)
 
 (* Pretty Printer *)
 
-let rec print_form =
-    function x ->
-        if is_var x then print_string (dest_var x)
-        else match dest_impl x with
-           (a, b) -> if is_impl a then (print_string "("; print_form a; print_string ") -> "; print_form b)
-                     else (print_form a; print_string " -> "; print_form b)
+let rec print_form x = if is_var x
+    then print_string (dest_var x)
+    else match dest_impl x with
+        (a, b) -> if is_impl a
+            then (print_string "("; print_form a; print_string ") -> "; print_form b)
+            else (print_form a; print_string " -> "; print_form b)
 
-let print_thm =
-    function t -> 
-    if empty (hyp t) then (print_string "|- "; print_form (concl t))
+let print_thm t = if empty (hyp t)
+    then (print_string "|- "; print_form (concl t))
     else (implode print_form ", " (hyp t); print_string " |- "; print_form (concl t))
+
+let print_thm_line t = print_thm t ; print_string "\n"
 
 (* PACKAGE FOR GOAL-DIRECTED PROOFS *)
 
@@ -124,14 +121,13 @@ let by (tac : tactic) ((agoals, athm) : goalstate) =
                         (* Create explicit implication from first goal *)
                         let athm = intro_rule (goal_to_form agoal) athm in
                             (* Eliminate first goal with justification from tactic, and finish *)
-                                (List.append bgoals (List.tl agoals), elim_rule athm bthm)
+                            (List.append bgoals (List.tl agoals), elim_rule athm bthm)
 
 (* TACTICS WHICH CORRESPOND TO KERNEL RULES *)
 
 let assumption = (
-    fun (asl, c) ->
-        if List.mem c asl then
-           ([], fun _ -> assume c)
+    fun (asl, c) -> if List.mem c asl
+        then ([], fun _ -> assume c)
         else failwith("assumption, does not contain the conclusion as hypothesis")
     : tactic)
 
@@ -143,13 +139,15 @@ let intro_tac = (
                     intro_rule a bthm)
     : tactic)
 
-let elim_tac =
-    fun a (asl, c) -> (
-        [ (asl, mk_impl a c); (asl, a) ],
-        fun thms ->
+let elim_tac = (
+    fun a (asl, c) -> 
+        let just = (fun thms ->
             let lthm = List.find (fun thm -> concl thm = mk_impl a c) thms in
                 let rthm = List.find (fun thm -> concl thm = a) thms in
-                    elim_rule lthm rthm)
+                    elim_rule lthm rthm
+        ) in
+            ([ (asl, mk_impl a c); (asl, a) ], just)
+    : form -> tactic)
 
 (* Tests *)
 
@@ -164,15 +162,7 @@ let g2 = ([], mk_impl a a)
 ;;
 
 let (_, t) = by assumption (by assumption (by (elim_tac a) (mk_init g1))) in
-    print_thm t;
-
-print_string "\n"
-
-;;
+    print_thm_line t;
 
 let (_, t) = by assumption (by intro_tac (mk_init g2)) in
-    print_thm t;
-
-print_string "\n"
-
-;;
+    print_thm_line t;
